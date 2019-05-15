0a8a27ff79e0638873deee03f7db68ce280f2021
Thomas Körner <t.koerner@heimrich-hannot.de>
2019-05-15 11:18:22 +0200

    v0.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,13 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## [0.2.1] - 2019-05-15
+
+* [FIX] encore bundle was not an optional dependency
+* [FIX] added missing utils bundle dependency
+
 ## [0.2.0] - 2019-05-14
 
 * structure tab helper now works with recursive tabs
-* FIX: first tab is now selected if no cached tab
-* FIX:  correct legacy config for encore bundle
+* [FIX] first tab is now selected if no cached tab
+* [FIX] correct legacy config for encore bundle
--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -18,11 +18,14 @@ use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Config\ConfigInterface;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
 use Contao\ManagerPlugin\Config\ConfigPluginInterface;
+use Contao\ManagerPlugin\Config\ContainerBuilder;
+use Contao\ManagerPlugin\Config\ExtensionPluginInterface;
 use HeimrichHannot\TabControlBundle\ContaoTabControlBundle;
 use HeimrichHannot\TwigTemplatesBundle\ContaoTwigTemplatesBundle;
+use HeimrichHannot\UtilsBundle\Container\ContainerUtil;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
-class Plugin implements BundlePluginInterface, ConfigPluginInterface
+class Plugin implements BundlePluginInterface, ConfigPluginInterface, ExtensionPluginInterface
 {
 
     /**
@@ -46,7 +49,23 @@ class Plugin implements BundlePluginInterface, ConfigPluginInterface
     public function registerContainerConfiguration(LoaderInterface $loader, array $managerConfig)
     {
         $loader->load('@ContaoTabControlBundle/Resources/config/services.yml');
-        $loader->load('@ContaoTabControlBundle/Resources/config/config.yml');
         $loader->load('@ContaoTabControlBundle/Resources/config/datacontainers.yml');
+    }
+
+    /**
+     * Allows a plugin to override extension configuration.
+     *
+     * @param string $extensionName
+     *
+     * @return array<string,mixed>
+     */
+    public function getExtensionConfig($extensionName, array $extensionConfigs, ContainerBuilder $container)
+    {
+        return ContainerUtil::mergeConfigFile(
+            'huh_encore',
+            $extensionName,
+            $extensionConfigs,
+            __DIR__.'/../Resources/config/config_encore.yml'
+        );
     }
 }
