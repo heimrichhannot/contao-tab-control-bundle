9937fc225ea65ff71bdd49a24c832f84c875258d
Eric Gesemann <e.gesemann@heimrich-hannot.de>
2025-06-18 10:52:56 +0200

    fix FrontendAsset implement ServiceSubscriberInterface

--- src/Asset/FrontendAsset.php
+++ src/Asset/FrontendAsset.php
@@ -10,33 +10,30 @@ namespace HeimrichHannot\TabControlBundle\Asset;
 
 use HeimrichHannot\EncoreContracts\PageAssetsTrait;
 use HeimrichHannot\UtilsBundle\Util\Utils;
+use Symfony\Contracts\Service\ServiceSubscriberInterface;
 
-class FrontendAsset
+class FrontendAsset implements ServiceSubscriberInterface
 {
     use PageAssetsTrait;
-
-    private Utils $utils;
 
     /**
      * FrontendAsset constructor.
      */
-    public function __construct(Utils $utils)
-    {
-        $this->utils = $utils;
-    }
+    public function __construct(private readonly Utils $utils) {}
 
     public function addFrontendAsset(): void
     {
-        if ($this->utils->container()->isFrontend()) {
-            $this->addPageEntrypoint('contao-tab-control-bundle', [
-                'TL_JAVASCRIPT' => [
-                    'huh_contao-tab-control-bundle' => 'bundles/heimrichhannottabcontrol/contao-tab-control-bundle.js',
-                ],
-                'TL_CSS' => [
-                    'huh_contao-tab-control-bundle' => 'bundles/heimrichhannottabcontrol/contao-tab-control-bundle.css',
-                ],
-            ]);
+        if (!$this->utils->container()->isFrontend()) {
+            return;
         }
 
+        $this->addPageEntrypoint('contao-tab-control-bundle', [
+            'TL_JAVASCRIPT' => [
+                'huh_contao-tab-control-bundle' => 'bundles/heimrichhannottabcontrol/contao-tab-control-bundle.js',
+            ],
+            'TL_CSS' => [
+                'huh_contao-tab-control-bundle' => 'bundles/heimrichhannottabcontrol/contao-tab-control-bundle.css',
+            ],
+        ]);
     }
 }
